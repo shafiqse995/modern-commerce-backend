afc77e7014b4a1086a15b1b5417a237e1c7c5562
shafiqse995 <muhammad.shafiq@stud.fra-uas.de>
2025-01-27 22:30:23 +0500

    add category and search filters for product list

--- products/views.py
+++ products/views.py
@@ -2,9 +2,10 @@
 
 from rest_framework.generics import ListAPIView, RetrieveAPIView  # type: ignore
 from rest_framework.pagination import PageNumberPagination  # type: ignore
-
+from rest_framework.filters import SearchFilter  # type: ignore
 from .models import Product, ProductCategory
 from .serializers import ProductCategorySerializer, ProductSerializer
+from django_filters import rest_framework as filters  # type: ignore
 
 
 class ProductListPagination(PageNumberPagination):
@@ -13,12 +14,25 @@ class ProductListPagination(PageNumberPagination):
     max_page_size = 100
 
 
+class ProductListFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
+    category = filters.BaseInFilter(field_name="category__id", lookup_expr="in")
+
+    class Meta:
+        model = Product
+        fields = ["min_price", "max_price", "category"]
+
+
 class ProductListAPIView(ListAPIView):
     """Product List API View"""
 
-    pagination_class = ProductListPagination
     queryset = Product.objects.all().order_by("-created_at")
+    pagination_class = ProductListPagination
     serializer_class = ProductSerializer
+    filter_backends = [SearchFilter, filters.DjangoFilterBackend]
+    search_fields = ["title", "description", "category__title"]
+    filterset_class = ProductListFilter
 
 
 product_list_view = ProductListAPIView.as_view()
