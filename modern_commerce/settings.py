b1ae93c581e1be4c64f2b43615ea424b61b8e814
shafiqse995 <muhammad.shafiq@stud.fra-uas.de>
2025-01-27 22:30:23 +0500

    connect to postgres for development

--- modern_commerce/settings.py
+++ modern_commerce/settings.py
@@ -83,9 +83,16 @@ WSGI_APPLICATION = "modern_commerce.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    },
 }
 
 
